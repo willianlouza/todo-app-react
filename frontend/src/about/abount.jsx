3186b8e145935ea3d61e6d55bd5a1c45a69b9e0b
Willian <willianlouza@gmail.com>
2022-06-28 18:44:55 -0300

    first commit

---
+++ frontend/src/about/abount.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import PageHeader from "../template/pageHeader";
+
+export default (props) => (
+  <div>
+    <PageHeader name="Sobre" small="To Do React App" />
+    <p>Aplicação web de tarefas feito para estudo utilizando React, MongoDB e Bootstrap.</p>
+  </div>
+)
---
+++ frontend/src/template/iconButton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import If from "./if";
+
+export default (props) => (
+  <If condition={!props.hide}>
+      <button className={"btn btn-" + props.style} onClick={props.onClick}>
+        <i className={"fa fa-" + props.icon}></i>
+      </button>
+  </If>
+)
